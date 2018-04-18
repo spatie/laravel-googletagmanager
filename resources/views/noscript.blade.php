9e4dd939536706258d2262ba1b6e2043510293f8
Zabanya <chaeril@live.com>
2018-04-19 03:19:52 +0700

    add noscript variable to gtm so it will fire when run

--- resources/views/noscript.blade.php
+++ resources/views/noscript.blade.php
@@ -1,5 +1,13 @@
 @if($enabled)
+@php
+$dLayer = "";
+
+foreach($noScript as $ns)
+$dLayer += "&" . $ns;
+endforeach
+
+@endphp
 @foreach($id as $item)
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $item }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $item }}{{ $dLayer }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @endforeach
 @endif
