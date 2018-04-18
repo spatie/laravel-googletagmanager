fe232bde6bcb1926c01cd43fb372e253e23d0d1d
Zabanya <chaeril@live.com>
2018-04-19 03:20:08 +0700

    datalayer rename

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -11,6 +11,6 @@ dataLayer.push({!! $item->toJson() !!});
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ $item }}');</script>
+})(window,document,'script','{{ $layer }}','{{ $item }}');</script>
 @endforeach
 @endif
