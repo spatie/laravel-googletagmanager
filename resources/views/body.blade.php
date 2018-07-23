b892e3beb0125ce0b77ab4abc42c37ecb83d2b2a
Sergey Fayngold <sergey@faynhost.com>
2018-07-23 21:35:06 +0300

    Separate the script to head and body sections like described in the latest GTM documentation while keeping backwards compatibility

---
+++ resources/views/body.blade.php
@@ -0,0 +1,4 @@
+@if($enabled)
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $id }}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+@endif
--- src/GoogleTagManagerServiceProvider.php
+++ src/GoogleTagManagerServiceProvider.php
@@ -23,7 +23,7 @@ class GoogleTagManagerServiceProvider extends ServiceProvider
         ], 'views');
 
         $this->app['view']->creator(
-            ['googletagmanager::script'],
+            ['googletagmanager::head', 'googletagmanager::body'],
             'Spatie\GoogleTagManager\ScriptViewCreator'
         );
     }
