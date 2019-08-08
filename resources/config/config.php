4f1ecb60c03e9affaccf8572e2238cbd1fba2ba2
Kristoffer Tonning <tonning@gmail.com>
2019-08-08 11:13:27 -0400

    Allow for config values to be set in the .env file

--- resources/config/config.php
+++ resources/config/config.php
@@ -5,23 +5,23 @@ return [
     /*
      * The Google Tag Manager id, should be a code that looks something like "gtm-xxxx".
      */
-    'id' => '',
+    'id' => env('GOOGLE_TAG_MANAGER_ID', ''),
     
     /*
      * Enable or disable script rendering. Useful for local development.
      */
-    'enabled' => true,
+    'enabled' => env('GOOGLE_TAG_MANAGER_ENABLED', true),
 
     /*
      * If you want to use some macro's you 'll probably store them
      * in a dedicated file. You can optionally define the path
      * to that file here and we will load it for you.
      */
-    'macroPath' => '',
+    'macroPath' => env('GOOGLE_TAG_MANAGER_MACRO_PATH', ''),
 
     /*
      * The key under which data is saved to the session with flash.
      */
-    'sessionKey' => '_googleTagManager',
+    'sessionKey' => env('GOOGLE_TAG_MANAGER_SESSION_KEY', '_googleTagManager'),
 
 ];
